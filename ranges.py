3389bddf6d19b8fb55348c1d783394b3fa143dde
tcreis <thiagoth.reis@gmail.com>
2018-03-27 22:57:34 -0500

    5th day

--- ranges.py
+++ ranges.py
@@ -1,12 +1,21 @@
 """Range exercises"""
 
 
-def get_factors():
+def get_factors(number):
     """Return a list of all factors of the given number."""
+    result = []
+    for x in range(1, number + 1):
+        if number % x == 0:
+            result.append(x)
+    return result
 
 
-def is_prime():
+def is_prime(number):
     """Return True if candidate number is prime."""
+    for x in range(2, number):
+        if number % x == 0:
+            return False
+    return True
 
 
 def power_list():
@@ -23,3 +32,8 @@ def is_range():
 
 def triples():
     """Return list of Pythagorean triples less than input num."""
+
+
+def prints(list):
+    for n in range(list):
+        print(n)
